1e65ccc46b4d8463cb3ab9c590c24f6510b13eed
Iasir-Arafat-Riaz <iasir.arafat.riaz@gmail.com>
2021-03-02 03:52:39 +0600

    company commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  var elonMask = {com:"spaceX", age:50, job:"tesla", status:"reach"}
+  var elonMask = {com:"spaceX", age:50, job:"tesla", status:"rich"}
+  var gates = {com:"microsoft", age:60, job:"ibm", status:"rich"}
   return (
     <div className="App">
       <header className="App-header">
@@ -11,7 +12,8 @@ function App() {
           Edit done <code>src/App.js</code> and save to reload.
         </p>
         <p>my first react app</p>
-        <h3>My heading : {elonMask.com+" "+elonMask.job}</h3>
+        <h3>Top Company : {elonMask.com+" "+elonMask.job}</h3>
+        <h3>Windows Operating system Provider:{gates.com}</h3>
       </header>
     </div>
   );
