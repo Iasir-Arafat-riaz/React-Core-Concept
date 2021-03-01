e34f329c58505f33f344aa18cfb0786e182ef13a
Iasir-Arafat-Riaz <iasir.arafat.riaz@gmail.com>
2021-03-02 04:27:57 +0600

    third commit

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  var elonMask = {com:"spaceX", age:50, job:"tesla", status:"rich"}
-  var gates = {com:"microsoft", age:60, job:"ibm", status:"rich"}
+  var elonMask = { com: "spaceX", age: 50, job: "tesla", status: "rich" }
+  var gates = { com: "microsoft", age: 60, job: "ibm", status: "rich" }
+  var coustom = {
+    color: "red",
+    backgroundColor: "blue"
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -12,8 +16,8 @@ function App() {
           Edit done <code>src/App.js</code> and save to reload.
         </p>
         <p>my first react app</p>
-        <h3>Top Company : {elonMask.com+" "+elonMask.job}</h3>
-        <h3>Windows Operating system Provider:{gates.com}</h3>
+        <h3 style={{color:"green"}}>Top Company : {elonMask.com + " " + elonMask.job}</h3>
+        <h3 style={coustom}>Windows Operating system Provider: {gates.com}</h3>
       </header>
     </div>
   );
