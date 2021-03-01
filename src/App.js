a0bd533d5cd811b5892b090c57d0213900fec64c
Iasir-Arafat-Riaz <iasir.arafat.riaz@gmail.com>
2021-03-02 03:47:02 +0600

    first

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var elonMask = {com:"spaceX", age:50, job:"tesla", status:"reach"}
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +10,8 @@ function App() {
         <p>
           Edit done <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>my first react app</p>
+        <h3>My heading : {elonMask.com+" "+elonMask.job}</h3>
       </header>
     </div>
   );
