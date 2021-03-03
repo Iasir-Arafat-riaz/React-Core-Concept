36e6d17ed8733d17265dd87b8a174fd0e11f0381
Iasir-Arafat-Riaz <iasir.arafat.riaz@gmail.com>
2021-03-03 06:41:41 +0600

    just commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  const array=["army","freedom Fighter","movement leader", "political leader",]
-  const products=[{name:"TV", price:1000}, {name:"mobile", price:500}, {name:"fridge", price:1200}, {name:"laptop", price:1500}]
- const displayArray = array.map(content=> content)
- console.log(displayArray)
+  const array = ["army", "freedom Fighter", "movement leader", "political leader",]
+  const products = [{ name: "TV", price: 1000 }, { name: "mobile", price: 500 }, { name: "fridge", price: 1200 }, { name: "laptop", price: 1500 }]
+  const displayArray = array.map(content => content)
+  console.log(displayArray)
 
   return (
     <div className="App">
       <header className="App-header">
+      <Counter></Counter>
         <ul>
-          {array.map(position=> <li>{position}</li>)}
-         </ul>
-         <ul>
-           {products.map(product=><div>{product.price}</div>)}
-         </ul>
-
-         {products.map(prod=><Product des={prod.name} prize={prod.price}></Product>)}
+          {array.map(position => <li>{position}</li>)}
+        </ul>
+        <ul>
+          {products.map(product => <div>{product.price}</div>)}
+        </ul>
         
+        {products.map(prod => <Product des={prod.name} prize={prod.price}></Product>)}
 
+        
         <Product des={products[0].name} prize={products[0].price}></Product>
         <Product des={products[1].name} prize={products[1].price}></Product>
         <Product des={products[2].name} prize={products[2].price}></Product>
@@ -27,20 +29,31 @@ function App() {
 
         <Product2 item={products[0]}></Product2>
         <Product2 item={products[1]}></Product2>
-      
-       
-       <Character name="mohammad mahmudur" desc="my university friend"></Character>
-       <Character name={array[0]} desc="our country hero"></Character>
-       <Character name = "toki" desc="classmate + crime partner"></Character>
-       <Character name="anjan" desc="school friend forever"></Character>
-        
+
+
+        <Character name="mohammad mahmudur" desc="my university friend"></Character>
+        <Character name={array[0]} desc="our country hero"></Character>
+        <Character name="toki" desc="classmate + crime partner"></Character>
+        <Character name="anjan" desc="school friend forever"></Character>
+
       </header>
     </div>
   );
 }
-function Product2(props){
-  const productStyle ={color:"red", backgroundColor:"silver", margin:"10px", borderRadius:"10px", padding:"10px", float:"left", width:"300px", fontSize:"30px", height:"300px" }
-  const {name, price}= props.item;
+function Counter() {
+  const [set, setCount]= useState(10);
+  const addHandler =()=>setCount(set+1); 
+  
+  return (<div>
+    <h1>count: {set}</h1>
+    <button onClick={()=>setCount(set-1)}>decrease</button>
+    <button onClick={addHandler}>increase</button>
+  </div>)
+}
+
+function Product2(props) {
+  const productStyle = { color: "red", backgroundColor: "silver", margin: "10px", borderRadius: "10px", padding: "10px", float: "left", width: "300px", fontSize: "30px", height: "300px" }
+  const { name, price } = props.item;
   return (<div style={productStyle} >
 
     <h2>{name}</h2>
@@ -49,8 +62,8 @@ function Product2(props){
   </div>)
 }
 
-function Product(props){
-  const proStyle ={color:"silver", backgroundColor:"gray", margin:"10px", borderRadius:"10px", padding:"10px", float:"left", width:"300px" }
+function Product(props) {
+  const proStyle = { color: "silver", backgroundColor: "gray", margin: "10px", borderRadius: "10px", padding: "10px", float: "left", width: "300px" }
   console.log(props)
   return (
     <div style={proStyle}>
@@ -62,13 +75,13 @@ function Product(props){
 
 }
 
-function Character(props){
-  let style= {color:"red" ,margin:"10px"}
+function Character(props) {
+  let style = { color: "red", margin: "10px" }
   return (
-  <div style={{border:"2px solid gold" , margin:"10px", borderRadius:"10px", padding:"10px"}}>
-       <h2 style={style}>{props.name}</h2>
-       <p>{props.desc}</p>
-  </div>)
+    <div style={{ border: "2px solid gold", margin: "10px", borderRadius: "10px", padding: "10px" }}>
+      <h2 style={style}>{props.name}</h2>
+      <p>{props.desc}</p>
+    </div>)
 }
 
 
